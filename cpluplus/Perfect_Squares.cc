a776353993afd4147873d9a5cc109778db1f5d42
yuandaxing <mryuan0@gmail.com>
2015-10-16 08:37:44 -0400

    perfect square and nim game problems

--- cpluplus/Perfect_Squares.cc
+++ cpluplus/Perfect_Squares.cc
@@ -35,14 +35,43 @@ using std::for_each;
 class Solution
 {
 public:
+
+  void numSquares(int left, int max_v, int count)
+  {
+    if(count >= min_)
+    {
+     return ;
+    }
+
+    if (left == 0)
+    {
+      min_ = std::min(count, min_);
+      return ;
+    }
+    int v = static_cast<int>(sqrt(left));
+    max_v = std::min(v, max_v);
+    numSquares(left - max_v*max_v, max_v, count+1);
+    if (max_v > 1)
+      numSquares(left, max_v-1, count);
+  }
   int numSquares(int n)
   {
-
+    numSquares(n, INT_MAX, 0);
+    return min_;
   }
+
+  Solution():min_(INT_MAX)
+  {
+  }
+private:
+  int min_;
 };
 
 int main()
 {
-
+  Solution sol;
+  cout << sol.numSquares(13) << endl;
+  Solution sol1;
+  cout << sol1.numSquares(12) << endl;
   return 0;
 }
