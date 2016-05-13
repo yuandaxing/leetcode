5789d90e8e1c55ce4c69774006cb3f8addc294da
yuandaxing <mryuan0@gmail.com>
2016-05-13 19:18:34 +0800

    update

---
+++ cpluplus/BestTimetoBuyandSellStockwithCooldown.cpp
@@ -0,0 +1,52 @@
+/*
+ * author: yuandx
+ * create: 2016-05-13
+ * email: yuandx@mvad.com
+ */
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <climits>
+#include <string>
+#include <utility>
+#include <functional>
+#include <algorithm>
+#include <stdio.h>
+#include <sstream>
+#include <unordered_map>
+#include <unordered_set>
+
+using std::unordered_map;
+using std::unordered_set;
+using std::ostringstream;
+using std::istringstream;
+using std::stringstream;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+using std::map;
+using std::set;
+using std::pair;
+using std::sort;
+using std::string;
+using std::for_each;
+/*
+ * S, B, C
+ * S[i] = max{B[0...i-1] + A[i]}
+ * B[i] = max{C[i-1] - A[i]}
+ * C[i] = max{S[i]}
+ */
+class Solution
+{
+public:
+
+};
+
+int main()
+{
+
+  return 0;
+}
