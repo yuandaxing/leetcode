2ea4f272aac7d2f3460065ccd4914357060025a8
yuandaxing <mryuan0@gmail.com>
2016-05-23 14:22:16 +0800

    local update

--- cpluplus/Course-Schedule-II.cc
+++ cpluplus/Course-Schedule-II.cc
@@ -9,33 +9,34 @@ using std::set;
 
 class Solution {
 public:
-    vector<int> findOrder(int numCourses, vector<pair<int, int> >& prerequisites) {
-      map<int, set<int> > pre_req;
-      vector<int> result;
-      for (int i = 0; i != numCourses; i++) pre_req[i] = set<int>();
-      for (auto it = prerequisites.begin(); it != prerequisites.end(); ++it)
-      {
-        pre_req[it->first].insert(it->second);
-      }
-      while (!pre_req.empty())
-      {
-        int find = numCourses;
-        for (map<int, set<int> >::iterator it = pre_req.begin(); it != pre_req.end(); ++it)
-        {
-          if( it->second.size() == 0)
-          {
-            find = it->first;
-            break;
-          }
-        }
-        if (find == numCourses) break; //not find
-        result.push_back(find);
-        pre_req.erase(find);
-        for (map<int, set<int> >::iterator it = pre_req.begin(); it != pre_req.end(); ++it)
-        {
-          it->second.erase(find);
-        }
-      }
-      return result.size() == static_cast<size_t>(numCourses) ? result : vector<int>();
+  vector<int> findOrder(int numCourses, vector<pair<int, int> >& prerequisites)
+  {
+    map<int, set<int> > pre_req;
+    vector<int> result;
+    for (int i = 0; i != numCourses; i++) pre_req[i] = set<int>();
+    for (auto it = prerequisites.begin(); it != prerequisites.end(); ++it)
+    {
+      pre_req[it->first].insert(it->second);
     }
+    while (!pre_req.empty())
+    {
+      int find = numCourses;
+      for (map<int, set<int> >::iterator it = pre_req.begin(); it != pre_req.end(); ++it)
+      {
+        if( it->second.size() == 0)
+        {
+          find = it->first;
+          break;
+        }
+      }
+      if (find == numCourses) break; //not find
+      result.push_back(find);
+      pre_req.erase(find);
+      for (map<int, set<int> >::iterator it = pre_req.begin(); it != pre_req.end(); ++it)
+      {
+        it->second.erase(find);
+      }
+    }
+    return result.size() == static_cast<size_t>(numCourses) ? result : vector<int>();
+  }
 };
