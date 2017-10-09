9ef98691c0d005e6e38b1cbda6c355c36849da3f
yuandaxing <mryuan0@gmail.com>
2017-10-09 09:39:21 +0800

    update

--- cpluplus/3Sum.cc
+++ cpluplus/3Sum.cc
@@ -26,67 +26,68 @@ using std::for_each;
 
 struct Triple
 {
-  int x, y, z;
-  Triple(int x1, int y1, int z1): x(x1), y(y1), z(z1)
-  {
-  }
+    int x, y, z;
+    Triple(int x1, int y1, int z1): x(x1), y(y1), z(z1)
+    {
+    }
 };
 bool operator<(const Triple& lhs, const Triple& rhs)
 {
-  if (lhs.x != rhs.x)
-    return lhs.x - rhs.x > 0;
-  if (lhs.y != rhs.y)
-    return lhs.y - rhs.y > 0;
-  if (lhs.z != rhs.z)
-    return lhs.z - rhs.z > 0;
-  return false;
+    if (lhs.x != rhs.x)
+        return lhs.x - rhs.x > 0;
+    if (lhs.y != rhs.y)
+        return lhs.y - rhs.y > 0;
+    if (lhs.z != rhs.z)
+        return lhs.z - rhs.z > 0;
+    return false;
 }
 
 class Solution
 {
 public:
-  vector<vector<int>> threeSum(vector<int>& nums)
-  {
-    std::multiset<int> nums_to_idx;
-    std::set<Triple> ret_set;
-    for (size_t i = 0; i != nums.size(); i++)
+    vector<vector<int>> threeSum(vector<int>& nums)
     {
-      nums_to_idx.insert(nums[i]);
-    }
-    for (auto it = nums_to_idx.begin(); it != nums_to_idx.end(); ++it)
-    {
-      auto it1 = it, it2 = --(nums_to_idx.end());
-      ++it1;
-      while (it1 != nums_to_idx.end() && it1 != it2)
-      {
-        int sum = *it + *it1 + *it2;
-        if (sum > 0)
-          --it2;
-        else if (sum < 0)
-          ++it1;
-        else
+        vector<vector<int>> ret;
+        std::sort(nums.begin(), nums.end());
+        int size = nums.size();
+        for(int i = 0; i < size; i++)
         {
-          ret_set.insert(Triple(*it, *it1, *it2));
-          ++it1;
+            if (i > 0 && nums[i]==nums[i-1])
+                continue;
+            for (int j = i+1, k = size -1; j < k; )
+            {
+                if (j > i+1 && nums[j] == nums[j-1])
+                {
+                    j++;
+                    continue;
+                }
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0)
+                {
+                    ret.push_back({nums[i], nums[j], nums[k]});
+                    j++, k--;
+                }
+                else if (sum < 0)
+                {
+                    j++;
+                }
+                else
+                {
+                    k--;
+                }
+            }
         }
-      }
+        return ret;
     }
-    vector<vector<int>> ret;
-    std::for_each(ret_set.begin(), ret_set.end(), [&ret](const Triple& e)
-                  {
-                    ret.push_back({e.x, e.y, e.z});
-                  });
-    return ret;
-  }
 };
 int main()
 {
-  Solution sol = Solution();
-  vector<int> input = {7,-1,14,-12,-8,7,2,-15,8,8,-8,-14,-4,-5,7,9,11,-4,-15,-6,1,-14,4,3,10,-5,2,1,6,11,2,-2,-5,-7,-6,2,-15,11,-6,8,-4,2,1,-1,4,-6,-15,1,5,-15,10,14,9,-8,-6,4,-6,11,12,-15,7,-1,-9,9,-1,0,-4,-1,-12,-2,14,-9,7,0,-3,-4,1,-2,12,14,-10,0,5,14,-1,14,3,8,10,-8,8,-5,-2,6,-11,12,13,-7,-12,8,6,-13,14,-2,-5,-11,1,3,-6};
-  for (auto & e : sol.threeSum(input))
-  {
-    cout << e[0] << " " << e[1] << " " << e[2] << "\t";
-  }
-  cout << endl;
-  return 0;
+    Solution sol = Solution();
+    vector<int> input = {7,-1,14,-12,-8,7,2,-15,8,8,-8,-14,-4,-5,7,9,11,-4,-15,-6,1,-14,4,3,10,-5,2,1,6,11,2,-2,-5,-7,-6,2,-15,11,-6,8,-4,2,1,-1,4,-6,-15,1,5,-15,10,14,9,-8,-6,4,-6,11,12,-15,7,-1,-9,9,-1,0,-4,-1,-12,-2,14,-9,7,0,-3,-4,1,-2,12,14,-10,0,5,14,-1,14,3,8,10,-8,8,-5,-2,6,-11,12,13,-7,-12,8,6,-13,14,-2,-5,-11,1,3,-6};
+    for (auto & e : sol.threeSum(input))
+    {
+        cout << e[0] << " " << e[1] << " " << e[2] << "\t";
+    }
+    cout << endl;
+    return 0;
 }
--- cpluplus/increasing_subsequence.cc
+++ cpluplus/increasing_subsequence.cc
@@ -39,14 +39,71 @@ using std::deque;
 class Solution
 {
 public:
-    vector<vector<int>> findSubsequences(vector<int>& nums) {
+    Solution():uniq(Solution::LT)
+    {
 
     }
+public:
+
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+        uniq.clear();
+        vector<vector<int>> ret;
+        for(int i = 1; i <nums.size(); i++)
+        {
+            int size = ret.size();
+            for (int k = 0; k < i; k++)
+            {
+                if (nums[i] < nums[k])
+                    continue;
+                vector<int> cur = {nums[k], nums[i]};
+                if (uniq.count(cur) == 0)
+                {
+                    ret.push_back(cur);
+                    uniq.insert(cur);
+                }
+            }
+            for (int k = 0; k < size; k++)
+            {
+                vector<int> cur = ret[k];
+                if (nums[i] < cur.back())
+                    continue;
+                cur.push_back(nums[i]);
+                if (uniq.count(cur) == 0)
+                {
+                    ret.push_back(cur);
+                    uniq.insert(cur);
+                }
+            }
+        }
+        return ret;
+    }
+    static bool LT(const vector<int>& v1, const vector<int>& v2)
+    {
+        int size = std::min(v1.size(), v2.size());
+        for (int i = 0; i < size; i++)
+        {
+            if (v1[i] != v2[i])
+                return v1[i] < v2[i];
+        }
+        return v1.size() < v2.size();
+    }
+private:
+    set<vector<int>, std::function<bool(const vector<int>&, const vector<int>&)>> uniq;
 };
 
 
 int main()
 {
-
+    vector<int> input = {4, 6, 7, 7};
+    Solution s;
+    auto ret = s.findSubsequences(input);
+    for (auto it = ret.begin(); it != ret.end(); ++it)
+    {
+        for (auto it1 = (*it).begin(); it1 != (*it).end(); ++it1)
+        {
+            cout << *it1 << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
